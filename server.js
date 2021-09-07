16738733337333b8cd6658961fc09166a06d749b
HarshUpadhyay27 <71074291+HarshUpadhyay27@users.noreply.github.com>
2021-09-08 02:06:24 +0530

    set up for project

---
+++ server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({ msg: "hello" });
+});
+
+const URL = process.env.MONGODB_URL;
+mongoose
+  .connect(URL, (err) => {
+    if(err) throw err;
+    console.log('connected to MongoDB')
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
