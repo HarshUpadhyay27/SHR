f4021b652e2e62b7ac52350c72951b9670f53583
HarshUpadhyay27 <71074291+HarshUpadhyay27@users.noreply.github.com>
2021-10-03 20:54:20 +0530

    adding update profile route

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -11,6 +11,40 @@ const userCtrl = {
       return res.status(500).json({ msg: error.message });
     }
   },
+  getUser: async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id).select("-password");
+      if (!user) return res.status(400).json({ msg: "User does not exist" });
+      res.json({ user });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  updateUser: async (req, res) => {
+    try {
+      const { fullname, avatar, mobile, address, story, website, gender } =
+        req.body;
+      if (!fullname)
+        return res.status(400).json({ msg: "Please add your full name." });
+
+      await User.findByIdAndUpdate(
+        { _id: req.user._id },
+        {
+          avatar,
+          fullname,
+          mobile,
+          address,
+          story,
+          website,
+          gender,
+        }
+      );
+
+      res.json({ msg: "Update Success!" });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
 };
 
-module.exports = userCtrl
+module.exports = userCtrl;
