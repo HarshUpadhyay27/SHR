9fa35942b6846042965cb48451620599661ef5fb
HarshUpadhyay27 <71074291+HarshUpadhyay27@users.noreply.github.com>
2021-10-23 23:06:38 +0530

    adding get post route

--- controllers/postCtrl.js
+++ controllers/postCtrl.js
@@ -22,6 +22,19 @@ const postCtrl = {
       return res.status(500).json({ msg: error.message });
     }
   },
+  getPosts: async (req, res)=>{
+    try {
+      const posts = await Posts.find({user: [...req.user.following, req.user._id]}).populate("user likes", "avatar username fullname")
+
+      res.json({
+        msg: "Success!",
+        result: posts.length,
+        posts
+      })
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  }
 };
 
 module.exports = postCtrl;
--- models/postModel.js
+++ models/postModel.js
@@ -7,7 +7,7 @@ const postSchema = new mongoose.Schema(
       type: Array,
       required: true
     },
-    like: [
+    likes: [
       {
         type: mongoose.Types.ObjectId,
         ref: "user",
