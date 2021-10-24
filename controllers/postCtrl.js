b7370845fa93601da276fb27779460e7b4d41ae0
HarshUpadhyay27 <71074291+HarshUpadhyay27@users.noreply.github.com>
2021-10-24 14:43:16 +0530

    adding like and unlike route

--- controllers/postCtrl.js
+++ controllers/postCtrl.js
@@ -63,6 +63,40 @@ const postCtrl = {
       return res.status(500).json({ msg: error.message });
     }
   },
+  likePost: async (req, res) => {
+    try {
+      const post = await Posts.find({
+        _id: req.params.id,
+        likes: req.user._id,
+      });
+      if (post.length>0) return res.status(500).json({ msg: "You likes this post" });
+
+      await Posts.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $push: { likes: req.user._id },
+        }
+      );
+
+      res.json({ msg: "Like Post" });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  unlikePost: async (req, res) => {
+    try {
+      await Posts.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $pull: { likes: req.user._id },
+        }
+      );
+
+      res.json({ msg: "UnLike Post" });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
 };
 
 module.exports = postCtrl;
