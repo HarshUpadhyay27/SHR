c026c3ddb640e8fc89e855a5e7de06270f9be2e2
HarshUpadhyay27 <71074291+HarshUpadhyay27@users.noreply.github.com>
2021-10-30 12:05:49 +0530

    adding like and update comment

--- controllers/commentCtrl.js
+++ controllers/commentCtrl.js
@@ -28,6 +28,55 @@ const commentCtrl = {
       return res.status(500).json({ msg: error.message });
     }
   },
+  updateComment: async (req, res) => {
+    try {
+      const { content } = req.body;
+
+      await Comments.findOneAndUpdate(
+        { _id: req.params.id, user: req.user._id },
+        { content }
+      );
+
+      res.json({ msg: "Update Success!" });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  likeComment: async (req, res) => {
+    try {
+      const comment = await Comments({
+        _id: req.params.id,
+        likes: req.user._id,
+      });
+      if (comment.length > 0)
+        return res.status(500).json({ msg: "You likes this comment" });
+
+      await Comments.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $push: { likes: req.user._id },
+        }
+      );
+
+      res.json({ msg: "Like Comment!" });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  unlikeComment: async (req, res) => {
+    try {
+      await Comments.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $pull: { likes: req.user._id },
+        }
+      );
+
+      res.json({ msg: "UnLike Comment!" });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
 };
 
 module.exports = commentCtrl;
--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -1,7 +1,11 @@
-const router = require('express').Router()
-const commentCtrl = require('../controllers/commentCtrl')
-const auth = require('../middleware/auth')
+const router = require("express").Router();
+const commentCtrl = require("../controllers/commentCtrl");
+const auth = require("../middleware/auth");
 
-router.post('/comment', auth, commentCtrl.createComment)
+router.post("/comment", auth, commentCtrl.createComment);
+router.patch("/comment/:id", auth, commentCtrl.updateComment);
 
-module.exports = router
+router.patch("/comment/:id/like", auth, commentCtrl.likeComment);
+router.patch("/comment/:id/unlike", auth, commentCtrl.unlikeComment);
+
+module.exports = router;
