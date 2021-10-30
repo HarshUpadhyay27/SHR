db45a160b2d4f6f78e2f072a427af3ef1942ff76
HarshUpadhyay27 <71074291+HarshUpadhyay27@users.noreply.github.com>
2021-10-30 15:42:22 +0530

    adding delete and reply route

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -7,5 +7,6 @@ router.patch("/comment/:id", auth, commentCtrl.updateComment);
 
 router.patch("/comment/:id/like", auth, commentCtrl.likeComment);
 router.patch("/comment/:id/unlike", auth, commentCtrl.unlikeComment);
+router.delete("/comment/:id", auth, commentCtrl.deleteComment);
 
 module.exports = router;
