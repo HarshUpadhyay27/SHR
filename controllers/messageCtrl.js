e6f77b62531e42b63644d79f2122d926a58544cf
HarshUpadhyay27 <harshupadhyay.ind@gmail.com>
2021-12-28 18:37:08 -0800

    adding save call time

--- controllers/messageCtrl.js
+++ controllers/messageCtrl.js
@@ -18,20 +18,21 @@ class APIfeatures {
 const messageCtrl = {
   createMessage: async (req, res) => {
     try {
-      const { recipient, text, media } = req.body;
-      if (!recipient || (!text.trim() && media.length === 0)) return;
+      const { sender, recipient, text, media, call } = req.body;
+      if (!recipient || (!text.trim() && media.length === 0 && !call)) return;
 
       const newConversation = await Conversations.findOneAndUpdate(
         {
           $or: [
-            { recipients: [req.user._id, recipient] },
-            { recipients: [recipient, req.user._id] },
+            { recipients: [sender, recipient] },
+            { recipients: [recipient, sender] },
           ],
         },
         {
-          recipients: [req.user._id, recipient],
+          recipients: [sender, recipient],
           text,
           media,
+          call,
         },
         {
           new: true,
@@ -41,7 +42,8 @@ const messageCtrl = {
 
       const newMessage = new Messages({
         conversation: newConversation._id,
-        sender: req.user._id,
+        sender,
+        call,
         recipient,
         text,
         media,
